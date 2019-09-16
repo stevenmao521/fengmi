2081624bb6417d80a554f676cc0dd59ff194fd99
maojianyu <mao@qq.com>
2019-09-16 11:06:00 +0800

    a

---
+++ app/bee/controller/Index.php
@@ -0,0 +1,26 @@
+<?php
+ /* 
+  * Copyright (C) 2017 All rights reserved.
+ *   
+ * @File UserTest.php
+ * @Brief 
+ * @Author 毛子
+ * @Version 1.0
+ * @Date 2017-12-26
+ * @Remark 首页
+ */
+namespace app\bee\controller;
+use think\Config;
+use think\Db;
+
+class Index extends Common {
+    
+    public function _initialize() {
+        parent::_initialize();
+    }
+    
+    public function index() {
+        
+        return $this->fetch('index');
+    }
+}
